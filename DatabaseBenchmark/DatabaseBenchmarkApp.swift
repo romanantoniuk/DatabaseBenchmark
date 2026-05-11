afddebe2189259e270c5857dd35394e28926423f
Roman Antoniuk <r.antoniuk.dev@gmail.com>
2026-05-11 17:50:49 +0300

    Initial Commit

---
+++ DatabaseBenchmark/DatabaseBenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseBenchmarkApp.swift
+//  DatabaseBenchmark
+//
+//  Created by Roman Antoniuk on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseBenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
